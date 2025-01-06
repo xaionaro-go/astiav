0bda27202c1ca5cd77620fb323a2eda7075fabc5
Dmitrii Okunev <xaionaro@dx.center>
2025-01-06 20:30:50 +0000

    Add a build tag to allow for pre-ffmpeg7 compatibility
    Functions `av_buffersink_get_colorspace` and `av_buffersink_get_color_range`
    do not exist in ffmpeg6.1:
        https://ffmpeg.org/doxygen/6.1/group__lavfi__buffersink__accessors.html
    while do exist in ffmpeg7.0:
        https://ffmpeg.org/doxygen/7.0/group__lavfi__buffersink__accessors.html#gaad817cdcf5493c385126e8e17c5717f2
    
    A similar problem is also with field `color_space` in AVBufferSrcParameters:
        https://ffmpeg.org/doxygen/7.0/structAVBufferSrcParameters.html
        https://ffmpeg.org/doxygen/6.1/structAVBufferSrcParameters.html
    
    To allow building with ffmpeg6, moving the functions into
    a file with a buildtag `ffmpeg7`.

--- buffersink_filter_context.go
+++ buffersink_filter_context.go
@@ -18,16 +18,6 @@ func (bfc *BuffersinkFilterContext) ChannelLayout() ChannelLayout {
 	return newChannelLayoutFromC(&cl)
 }
 
-// https://ffmpeg.org/doxygen/7.0/group__lavfi__buffersink__accessors.html#gab80976e506ab88d23d94bb6d7a4051bd
-func (bfc *BuffersinkFilterContext) ColorRange() ColorRange {
-	return ColorRange(C.av_buffersink_get_color_range(bfc.fc.c))
-}
-
-// https://ffmpeg.org/doxygen/7.0/group__lavfi__buffersink__accessors.html#gaad817cdcf5493c385126e8e17c5717f2
-func (bfc *BuffersinkFilterContext) ColorSpace() ColorSpace {
-	return ColorSpace(C.av_buffersink_get_colorspace(bfc.fc.c))
-}
-
 func (bfc *BuffersinkFilterContext) FilterContext() *FilterContext {
 	return bfc.fc
 }
